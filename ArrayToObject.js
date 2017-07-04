62477520a9acc9d3d2276026e5ea3e5b44b0a2a4
JordanBeziau <jordanbeziau@gmail.com>
2017-07-04 19:21:03 +0200

    Syntax update

--- ArrayToObject.js
+++ ArrayToObject.js
@@ -12,15 +12,10 @@ const votes = [
 const initiateValue = {};
 
 const reducer = (prev, vote) => {
-  if (!prev[vote]) {
-    prev[vote] = 1;
-  } else {
-    prev[vote]++;
-  }
+  !prev[vote] ? prev[vote] = 1 : prev[vote] ++;
   return prev;
 };
 
-const result = votes
-  .reduce(reducer, initiateValue);
+const result = votes.reduce(reducer, initiateValue);
 
 console.log(result); // { angular: 2, vue: 2, react: 3, ember: 1 }
